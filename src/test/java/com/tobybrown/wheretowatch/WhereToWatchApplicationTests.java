b2ea9047ddc3fb21bc206b076f11b8bc475cfe6c
Toby Brown <32519739+TobyBBrown@users.noreply.github.com>
2019-11-10 20:54:48 +0000

    Setup project using Spring Initializr

---
+++ src/test/java/com/tobybrown/wheretowatch/WhereToWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobybrown.wheretowatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereToWatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
